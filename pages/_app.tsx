f4a95dbdf093b35fc304fe0434d9ea6a4c6632d2
Mateusbrbza <mateus.c.barboza@gmail.com>
2023-04-07 14:53:20 -0300

    feat: finished auth UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import type { AppProps } from 'next/app';
 
 import Layout from '@/components/Layout';
-// import Modal from '@/components/Modal';
 import '@/styles/globals.css';
+import LoginModal from '@/components/modals/LoginModal';
+import Modal from '@/components/Modal';
+import RegisterModal from '@/components/modals/RegisterModal';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+    <RegisterModal />
+    <LoginModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
