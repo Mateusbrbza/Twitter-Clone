b8eabd300c287f24524f7e33a4add36805352f85
Mateusbrbza <mateus.c.barboza@gmail.com>
2023-04-10 15:55:00 -0300

    feat: finished user profile

---
+++ components/users/Userhero.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image';
+
+import useUser from '@/hooks/useUser';
+
+import Avatar from "../Avatar";
+
+interface UserHeroProps { 
+    userId: string;
+}
+
+const Userhero: React.FC<UserHeroProps> = ({ userId }) => {
+    const { data: fetchedUser } = useUser(userId);
+
+  return (
+    <div>
+        <div className="bg-neutral-700 h-44 relative">
+            {fetchedUser?.coverImage && (
+                <Image 
+                src={fetchedUser.coverImage} 
+                fill
+                alt="cover image"
+                style={{ objectFit: 'cover'}}
+                />
+            )}
+            <div className="absolute -bottom-16 lef-4">
+                <Avatar
+                userId={userId}
+                isLarge
+                hasBorder
+                />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Userhero
