1e1df0b332b1790dc29c52c59677e555a251df94
Mateusbrbza <mateus.c.barboza@gmail.com>
2023-04-11 21:29:52 -0300

    feat: load and create posts added to application

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -11,6 +11,49 @@ export default async function handler( req: NextApiRequest, res: NextApiResponse
     try {
         if (req.method === 'POST') {
             const { currentUser } = await serverAuth(req, res);
+            const { body } = req.body;
+
+            const post = await prisma.post.create({
+                data: {
+                    body,
+                    userId: currentUser.id,
+                }
+            });
+
+            return res.status(200).json(post);
+        }
+
+        if ( req.method === 'GET') {
+            const { userId } = req.query;
+
+            let posts;
+
+            if (userId && typeof userId === 'string') {
+                posts = await prisma.post.findMany({
+                    where: {
+                        userId
+                    },
+                    include: {
+                        user: true,
+                        comments: true
+                    },
+                    orderBy: {
+                        createdAt: 'desc'
+                    }
+                });
+            } else {
+                posts = await prisma.post.findMany({
+                    include: {
+                        user: true,
+                        comments: true
+                    }, 
+                    orderBy: {
+                        createdAt: 'desc'
+                    }
+                });
+            }
+
+            return res.status(200).json(posts);
         }
     } catch (err) {
         console.log(err);
