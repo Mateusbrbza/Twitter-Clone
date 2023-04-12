01e71a2fa0e2f18621bcd614e3e4b4993bd80d6e
Mateusbrbza <mateus.c.barboza@gmail.com>
2023-04-12 15:15:41 -0300

    feat: fixed components and hooks, added like functionality to posts

--- hooks/useFollow.ts
+++ hooks/useFollow.ts
@@ -1,9 +1,10 @@
+import axios from "axios";
 import { useCallback, useMemo } from "react";
+import { toast } from "react-hot-toast";
+
 import useCurrentUser from "./useCurrentUser";
 import useLoginModal from "./useLoginModal";
 import useUser from "./useUser";
-import { toast } from "react-hot-toast";
-import axios from "axios";
 
 const useFollow = (userId: string) => {
     const { data: currentUser, mutate: mutateCurrentUser } = useCurrentUser();
@@ -15,44 +16,36 @@ const useFollow = (userId: string) => {
         const list = currentUser?.followingIds || [];
 
         return list.includes(userId);
-    }, [userId, currentUser?.followingIds]);
+    }, [currentUser, userId]);
 
     const toggleFollow = useCallback(async () => {
         if (!currentUser) {
-            return loginModal.onOpen();
-        };
+        return loginModal.onOpen();
+        }
 
         try {
-            let request;
+        let request;
 
-            if (isFollowing) {
-                request = () => axios.delete('/api/follow', { data: { userId }});
-            } else {
-                request = () => axios.post('/api/follow', { userId });
-            }
-
-            await request();
-
-            mutateCurrentUser();
-            mutateFetchedUser();
-
-            toast.success('Success')
-        } catch(error) {
-            toast.error('Something went wrong');
+        if (isFollowing) {
+            request = () => axios.delete('/api/follow', { data: { userId } });
+        } else {
+            request = () => axios.post('/api/follow', { userId });
         }
-    }, [
-        currentUser, 
-        isFollowing, 
-        userId, 
-        mutateCurrentUser,
-        mutateFetchedUser,
-        loginModal
-    ]);
+
+        await request();
+        mutateCurrentUser();
+        mutateFetchedUser();
+
+        toast.success('Success');
+        } catch (error) {
+        toast.error('Something went wrong');
+        }
+    }, [currentUser, isFollowing, userId, mutateCurrentUser, mutateFetchedUser, loginModal]);
 
     return {
         isFollowing,
         toggleFollow,
     }
-};
+}
 
 export default useFollow;
